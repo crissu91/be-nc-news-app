ca347cbf42a9fd845a6502dcc38480e342fa7ba2
crissu91 <cristinaf.stefan@gmail.com>
2023-06-27 13:14:28 +0100

    add error file updated for custom errors

--- errors/error-handling.js
+++ errors/error-handling.js
@@ -11,6 +11,5 @@ exports.handleCustomErrors = (err, req, res, next) => {
 };
 
 exports.handleServerErrors = (err, req, res, next) => {
-    console.log(err)
     res.status(500).send({msg: - "Err from handle500s error- handler"});
 };
