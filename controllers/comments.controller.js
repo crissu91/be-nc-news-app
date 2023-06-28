76e69cca4149870aaaff82947d9c72f7928d730b
crissu91 <cristinaf.stefan@gmail.com>
2023-06-28 11:39:01 +0100

    add controller for comments to check article id exists

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,8 +1,16 @@
+const { checkArticleIdExists } = require("../models/articles.models");
 const { selectAllCommentsByArticleId } = require("../models/comments.models")
 
 exports.getCommentsByArticleId = (req, res, next) => {
     const { article_id } = req.params;
-    selectAllCommentsByArticleId(article_id).then((comments)=>{
+    const commentsPromise = [selectAllCommentsByArticleId(article_id)]
+
+    if (article_id) {
+        commentsPromise.push(checkArticleIdExists(article_id))
+    }
+    Promise.all(commentsPromise)
+    .then((resolvedPromises)=>{
+        const comments = resolvedPromises[0]
         res.status(200).send({comments})
     })
     .catch(next)
