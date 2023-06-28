4ab375bec504034d518d9f62b209eda289823bd3
crissu91 <cristinaf.stefan@gmail.com>
2023-06-28 11:39:39 +0100

    add models for article with check article id exists function

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,10 +1,24 @@
 const db = require('../db/connection');
 
+exports.checkArticleIdExists = (article_id) => {
+    if (/\D+/.test(article_id)) {
+        return Promise.reject ({ status: 400, msg:"Article ID is invalid"});
+    }
+    else if (/\d+/.test(article_id)) {
+        return db.query("SELECT * FROM articles WHERE article_id = $1", [article_id]).then(({rows})=> {
+            if (rows.length === 0) {
+                return Promise.reject({status: 404, msg: "Article not found."})
+            }
+            else return rows
+        })
+    }
+}
+
 exports.selectArticlesById = (article_id) =>{
     return db.query("SELECT * FROM articles WHERE article_id = $1",[article_id]).then(({rows}) => {
         if (rows.length === 0) {
             return Promise.reject({status: 404, msg: "Article not found."})
         }
-        else return rows
+        return rows
         })
 }
