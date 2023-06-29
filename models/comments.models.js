0a679ca77ef5105d72d149b503978ba808a5a629
crissu91 <cristinaf.stefan@gmail.com>
2023-06-29 11:34:22 +0100

    add controller andf models files for task 7 with answers

--- models/comments.models.js
+++ models/comments.models.js
@@ -8,3 +8,17 @@ exports.selectAllCommentsByArticleId = (article_id) =>{
         else return rows
     })
 }
+
+exports.insertCommentByArticleId = (body, article_id) => {
+    const queryString = `INSERT INTO comments (body, author, article_id) VALUES ($1, $2, $3) RETURNING *;`;
+    const queryValues = [body.body, body.username, article_id]
+
+    if (!body.body) {
+        return Promise.reject({status: 400, msg: 'Missing data'})
+    }
+
+    return db.query(queryString, queryValues).then((comment)=>{
+        return comment.rows
+    })
+}
+
