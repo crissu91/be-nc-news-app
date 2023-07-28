b08b719a3da285932fbacf88916697f7b6e8255e
crissu91 <cristinaf.stefan@gmail.com>
2023-07-28 17:47:15 +0100

    add controller and models for users/username

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,4 @@
-const { selectAllUsers } = require("../models/users.models")
+const { selectAllUsers, postUser, selectUserByUsername } = require("../models/users.models")
 
 exports.getAllUsers = (req, res, next) => {
     return selectAllUsers()
@@ -6,4 +6,23 @@ exports.getAllUsers = (req, res, next) => {
         res.status(200).send({users})
     })
     .catch(next)
+}
+
+exports.getUserByUsername = (req, res, next) => {
+	const username = req.params.username;
+	selectUserByUsername(username)
+		.then((user) => {
+			res.status(200).send({ user });
+		})
+		.catch(next);
+}
+
+exports.postNewUser = (req, res, next) => {
+	const body = req.body;
+
+	postUser(body)
+		.then((postedUser) => {
+			res.status(201).send({ postedUser });
+		})
+		.catch(next);
 }
--- models/users.models.js
+++ models/users.models.js
@@ -13,3 +13,28 @@ exports.selectAllUsers = () => {
         return rows
     })
 }
+exports.selectUserByUsername = (username) => {
+	return db
+		.query(`SELECT * FROM users WHERE username = $1`, [username])
+		.then((users) => {
+			if (users.rows.length === 0) {
+				return Promise.reject({ status: 404, msg: 'Invalid username' });
+			}
+			return users.rows;
+		});
+}
+exports.postUser = (body) => {
+	return db
+	.query(`SELECT * FROM users WHERE username = $1`, [body.username])
+	.then((result) => {
+		if (result.rows.length === 3) {
+			return Promise.reject({ status: 404, msg: 'Invalid username' });
+		}
+	}).then(() => {
+
+		return db
+			.query(`INSERT INTO users (username, name,	avatar_url) VALUES ($1, $2, $3) RETURNING*;`, [body.username, body.name, body.avatar_url])
+	}).then(({rows}) => {
+		return rows;
+	})
+}
